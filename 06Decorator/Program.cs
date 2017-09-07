807b250d2b7cf8929750901c75b6b45454e828e7
bluesmao <516870524@qq.com>
2017-09-07 23:04:02 +0800

    装饰器模式代码完善
    装饰器模式

--- 06Decorator/Program.cs
+++ 06Decorator/Program.cs
@@ -10,7 +10,10 @@ namespace _06Decorator
     {
         private string name;
 
-        public Person() { }
+        public Person()
+        {
+
+        }
 
         public Person(string name)
         {
@@ -25,11 +28,102 @@ namespace _06Decorator
 
     class Finery : Person
     {
-        private Person person;
+        protected Person component;
 
         public Finery()
         {
+        }
 
+        public void Decorate(Person component)
+        {
+            this.component = component;
+        }
+
+        public override void Show()
+        {
+            if (this.component != null)
+            {
+                component.Show();
+            }
+        }
+    }
+
+    class TShirts : Finery
+    {
+        public TShirts()
+        {
+
+        }
+
+        public override void Show()
+        {
+            Console.Write("大T恤 ");
+            base.Show();
+        }
+    }
+
+    class BigTrouser : Finery
+    {
+        public BigTrouser()
+        {
+
+        }
+        public override void Show()
+        {
+            Console.Write("垮裤 ");
+            base.Show();
+        }
+    }
+
+    class Sneakers : Finery
+    {
+        public Sneakers()
+        {
+
+        }
+        public override void Show()
+        {
+            Console.Write("破球鞋 ");
+            base.Show();
+        }
+    }
+
+    class Suit : Finery
+    {
+        public Suit()
+        {
+
+        }
+        public override void Show()
+        {
+            Console.Write("西装 ");
+            base.Show();
+        }
+    }
+
+    class Tie : Finery
+    {
+        public Tie()
+        {
+
+        }
+        public override void Show()
+        {
+            Console.Write("领带 ");
+            base.Show();
+        }
+    }
+
+    class LeatherShoes : Finery
+    {
+        public LeatherShoes()
+        {
+
+        }
+        public override void Show()
+        {
+            Console.Write("皮鞋 ");
+            base.Show();
         }
     }
 
@@ -37,6 +131,32 @@ namespace _06Decorator
     {
         static void Main(string[] args)
         {
+            Person person = new Person("小新");
+
+            Console.WriteLine("第一种装扮：");
+
+            Sneakers sk = new Sneakers();
+            BigTrouser bt = new BigTrouser();
+            TShirts ts = new TShirts();
+
+            sk.Decorate(person);
+            bt.Decorate(sk);
+            ts.Decorate(bt);
+
+            ts.Show();
+
+            Console.WriteLine("第二种装扮：");
+
+            LeatherShoes ls = new LeatherShoes();
+            Tie tie = new Tie();
+            Suit suit = new Suit();
+            ls.Decorate(person);
+            tie.Decorate(ls);
+            suit.Decorate(tie);
+
+            suit.Show();
+
+            Console.Read();
         }
     }
 }
